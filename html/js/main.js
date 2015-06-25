5bca17ed26323a162672814f4359685e68327bde
Nichlas Samuelsen <nichlas.samuelsen2@gmail.com>
2015-06-25 06:52:25 +0200

    New pages

---
+++ html/js/main.js
@@ -0,0 +1,15 @@
+var main = function() {
+    $('.non-active').click(function(){
+    	if( $(this).hasClass("active") ){
+    		$(this).removeClass();
+    		$(this).addClass("non-active");
+    	}
+    	else {
+    		$(".active").removeClass();
+    		$(this).addClass('active');
+    	}
+    	}
+    );
+};
+
+$(document).ready(main);
